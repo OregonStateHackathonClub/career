d3f3315a92001a7fd40be93aae06cc7dfc6fe5b8
owenkrause <owenjkrause@gmail.com>
2025-07-23 11:36:00 +0300

    fix: build errs

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 
-// Add type to globalThis for prisma to avoid type errors in development
 declare global {
-  // eslint-disable-next-line no-var
   var prisma: PrismaClient | undefined;
 }
 
