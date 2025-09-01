c49da8c216ba7b43abcfc5972eb58479a20b23d7
Faith Nambasa <nambasaf@oregonstate.edu>
2025-09-01 10:33:17 -0700

    fixed resume-preview

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,17 +1,8 @@
 "use client";
 import { Navbar } from "@/components/navbar";
-import { authClient } from "@/lib/auth-client";
-import { useRouter } from "next/navigation";
-import { useState } from "react";
 
 export default function SignInPage() {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [error, setError] = useState("");
-    const [success, setSuccess] = useState("");
-
-    const router = useRouter();
+    
     
     return (
         <>
